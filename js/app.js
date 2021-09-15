adbe95ef5f6da3d97fa8792d1019df520b359b57
Rubén Contreras <SteelMods@hotmail.com>
2021-09-15 03:08:45 -0500

    Logica casi terminada falta linea 40 app.js

--- js/app.js
+++ js/app.js
@@ -6,15 +6,42 @@ let porcentajeDescuento;
 let valorDescuento
 
 //FUNCIONES
+
 function calcularValorDescuento(){
-    let input = document.getElementById("inputPrize");
-    precioOriginal = Number(input.value);
-    const resultP =document.getElementById("calcularValorDescuento()");
-    input = document.getElementById("inputDiscount");
-    porcentajeDescuento = Number(input.value);
+    precioOriginal = document.getElementById("inputPrize").value;
+    porcentajeDescuento = document.getElementById("inputDiscount").value;
     valorDescuento = (porcentajeDescuento * precioOriginal) / 100;
     precioConDescuento = precioOriginal - valorDescuento;
-    input = document.getElementById("calculadora-resultado")
-    let pResultado = input;
-    pResultado.innerHTML = `El precio con descuento es ${precioConDescuento}.`    
+    pResultado = document.getElementById("calculadora-resultado");
+    pResultado.innerHTML = `El precio con descuento es $${precioConDescuento}.`;
+    comparar();
+};
+
+function Capturar(){
+    function Coupon(inputname,inputdiscount){
+        this.inputname = inputname ;  
+        this.inputdiscount = inputdiscount ;
+    };
+    
+    codeCoupon = document.getElementById("addCouponName").value;
+    numberDiscound = document.getElementById("addCouponDiscount").value;
+    
+    couponNew = new Coupon(codeCoupon,numberDiscound);
+    agregar();
+}
+couponsList = [];
+function agregar(){
+    couponsList.push(couponNew);
+    document.getElementById("add").innerHTML += `<p class="newcoupon__p">Código: ${couponNew.inputname} Descuento: ${couponNew.inputdiscount}</p>`;
+};
+function comparar(){
+    CouponDiscount = undefined;
+    code = document.getElementById("inputCoupon").value;
+    let compare = couponsList.find ( item => item.inputname == code);
+    if(code == ""){
+        console.log("No hay cupón")
+    };
+    else if (code === compare.inputname){
+        CouponDiscount = compare.inputdiscount;
+    };
 };
