dec0a7b84d796ee0f30be004712590865483995a
Rubén Contreras <SteelMods@hotmail.com>
2021-09-15 15:10:51 -0500

    Lógica terminada falta estilar la web

--- js/app.js
+++ js/app.js
@@ -4,17 +4,18 @@ let precioOriginal;
 let precioConDescuento;
 let porcentajeDescuento;
 let valorDescuento
+couponsList = [];
+let     CouponDiscount = undefined;
 
 //FUNCIONES
 
-function calcularValorDescuento(){
-    precioOriginal = document.getElementById("inputPrize").value;
-    porcentajeDescuento = document.getElementById("inputDiscount").value;
-    valorDescuento = (porcentajeDescuento * precioOriginal) / 100;
-    precioConDescuento = precioOriginal - valorDescuento;
-    pResultado = document.getElementById("calculadora-resultado");
-    pResultado.innerHTML = `El precio con descuento es $${precioConDescuento}.`;
-    comparar();
+function agregar(){
+    couponsList.push(couponNew);
+    document.getElementById("add").innerHTML += 
+    `<tr class="newcoupon-table-tr" id="newCouponstr">
+    <td class="newcoupon-tr__td">${couponNew.inputname}</td>
+    <td class="newcoupon-tr__td">${couponNew.inputdiscount}</td>
+    </tr>`;
 };
 
 function Capturar(){
@@ -29,16 +30,9 @@ function Capturar(){
     couponNew = new Coupon(codeCoupon,numberDiscound);
     agregar();
 }
-couponsList = [];
-function agregar(){
-    couponsList.push(couponNew);
-    document.getElementById("add").innerHTML += `<p class="newcoupon__p">Código: ${couponNew.inputname} Descuento: ${couponNew.inputdiscount}</p>`;
-};
 function comparar(){
-    //CouponDiscount = undefined;
     code = document.getElementById("inputCoupon").value;
     let compare = couponsList.find ( item => item.inputname == code);
-    CouponDiscount = undefined;
     if (code == ""){
         console.log("No hay cupón");
         console.log(CouponDiscount);
@@ -47,4 +41,26 @@ function comparar(){
         CouponDiscount = compare.inputdiscount;
         console.log(CouponDiscount);
     };
+};
+function calcularValorDescuento(){
+    comparar();
+    precioOriginal = document.getElementById("inputPrize").value;
+    porcentajeDescuento = document.getElementById("inputDiscount").value;
+    console.log(CouponDiscount);
+    pResultado = document.getElementById("calculadora-resultado");
+    if(CouponDiscount === undefined){
+        valorDescuento = (porcentajeDescuento * precioOriginal) / 100;
+        console.log(valorDescuento);
+        precioConDescuento = precioOriginal - valorDescuento;
+        pResultado.innerHTML = `El precio con descuento es $${precioConDescuento}.`;
+    }
+    else{
+        //console.log(`(${CouponDiscount} * ((${porcentajeDescuento} * ${precioOriginal})/100))/100`);
+        valorDescuento = (porcentajeDescuento * precioOriginal)/100;
+        //console.log(valorDescuento);
+        precioConDescuento = precioOriginal - valorDescuento;
+        otherDiscount = (precioConDescuento * CouponDiscount)/100; 
+        precioFinal = precioConDescuento - otherDiscount;
+        pResultado.innerHTML = `El precio con descuento es $${precioFinal}.`;
+    }
 };
